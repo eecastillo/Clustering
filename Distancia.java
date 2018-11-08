65744620d2da6dcb62559df4cf23e0620aab8de8
eecastillo <eduardo_ethandrake@yahoo.com.mx>
2018-11-08 12:03:00 -0600

    d

--- Distancia.java
+++ Distancia.java
@@ -1,12 +1,42 @@
 public class Distancia{
-    private double[] normalizados;
+    private double[] elementos;
     private DistanciaTipo tipo= DistanciaTipo.EUCLIDEAN;
-
-    public Distancia(){
+    private double distancia;
+  
+    public Distancia(double [] elementos ){
+      setElementos(elementos);
+      calcularDistancia();
     }
 
-    public Distancia(int ){
+    public Distancia(double [] elementos, DistanciaTipo tipo){
+      Distancia(elementos);
+      setTipo(tipo);
+    }
 
+    public void setElementos(double [] elementos){
+      this.elementos=elementos;
+    }
+    public void setTipo(DistanciaTipo tipo){
+      this.tipo=tipo;
+    }
+    public void setDistancia(){
+
+    }
+
+    public double[] getElementos(){
+      return this.elementos;
+    }
+    public DistanciaTipo getTipo(){
+      return this.tipo;
+    }
+    public double getDistancia(){
+      return this.distancia;
+    }
+
+    private void calcularDistancia(){
+      for(int i=0;i<elementos.length;i++){
+        
+      }
     }
 
 }
