964f371fd43a138fc145814a1f878dc4301065b7
eecastillo <eduardo_ethandrake@yahoo.com.mx>
2018-11-11 14:42:15 -0700

    inicio del modelo de num/char

--- MarkII/Clustering.java
+++ MarkII/Clustering.java
@@ -2,6 +2,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 //import Cluster;
 public class Clustering{
+
+    public static String[] Hamming(String[] arr){
+        String [] h=new String[5];
+        return h;
+    }
+    
+    public static double[][] sentido(String [][] matriz){
+        int[] arrIsInteger = new int[matriz[0].length];//1: entero, 2:string
+       // double[] minIntvalue= new double[matriz[0].length]; 
+        double[] ASCIIValue;
+        double [][] matrix=new double[matriz.length][matriz[0].length];
+        for(int j=0;j<matriz[0].length;j++){
+           // double min =Double.MAX_VALUE;
+            for(int i=0;i<matriz.length;i++){
+                switch(arrIsInteger[j]){
+                    case 0:
+                        try{
+                            matrix[i][j]=Double.valueOf(matriz[i][j]);
+                            arrIsInteger[j]=1;
+                
+                        }catch(NumberFormatException e){
+                            String word=matriz[i][j].toLowerCase();
+                            for(int p=0;p<word.length();p++){
+                                matrix[i][j]+= (double)word.charAt(p);;
+                            }
+                            arrIsInteger[j]=2;
+                        }
+                    break;
+                    case 1:
+                        matrix[i][j]=Double.valueOf(matriz[i][j]);
+                    break;
+                    case 2:
+                        String word=matriz[i][j].toLowerCase();
+                        for(int p=0;p<word.length();p++){
+                            matrix[i][j]+= (double)word.charAt(p);;
+                        }
+                       
+                    break;
+                }
+               /* if(arrIsInteger[j]==1 && matrix[i][j]<min){
+                    min=matrix[i][j];
+                    System.out.println(min);
+                }*/
+               
+            }
+            /*if(arrIsInteger[j]==1){
+                minIntvalue[j]=min;
+                for(int n=0;n<matriz.length;n++){
+                    matrix[n][j]-=minIntvalue[j];
+                }
+            }*/
+        }
+        //for(int k=0;k<matrix.length;k++)
+        //    System.out.println(Arrays.toString(matrix[k]));
+        
+        return matrix;
+    }
    
     public static void main(String[] args){
         //creacion de dos clusters por separado para despues convertirse en uno solo
@@ -29,7 +86,7 @@ public class Clustering{
         System.out.println(Arrays.toString(Trunks.getCentroide()));
         System.out.println(Arrays.toString(Goten.getCentroide()));
         System.out.println(Arrays.toString(Gotenks.getCentroide()));*/
-        ArrayList<Cluster> clust = new ArrayList<Cluster>();
+       /* ArrayList<Cluster> clust = new ArrayList<Cluster>();
         double[] data={12,1.98,1,2,4};
         Cluster Goten = new Cluster(data);
         double[] data2={11,1.85,1,5,4};
@@ -49,7 +106,24 @@ public class Clustering{
         System.out.printf("Distancia entre goten y trunks es: %f\n",distance1);
         System.out.printf("Distancia entre goten y goku es: %f\n",distance2);
         System.out.printf("Distancia entre goku y trunks es: %f\n",distance3);
+*/ 
+        String [][] alumnos ={{"15","perros"},
+                              {"15","Perra"},
+                              {"16","PERRas"},
+                              {"14","PerrO"},
+                              {"13","PeRrO"}};
+                              System.out.println(alumnos.length);
+                              System.out.println(alumnos[0].length);
+        double [][] tabla = sentido(alumnos);
+        for(int k=0;k<tabla.length;k++)
+            System.out.println(Arrays.toString(tabla[k]));
+        Cluster c0 = new Cluster(tabla[0]);
+        Cluster c1 = new Cluster(tabla[1]);
+        Cluster c2 = new Cluster(tabla[2]);
+        Cluster c3 = new Cluster(tabla[3]);
+        Cluster c4 = new Cluster(tabla[4]);
 
+       
 
     }
 }
--- MarkII/Elemento.java
+++ MarkII/Elemento.java
@@ -2,16 +2,20 @@ import java.util.Arrays;
 
 public class Elemento{
     //arreglo con los datos sin normalizar de un elemento
-    private double[] categorias;
+    private double[] datosNumericos;
+    private double[][] datosASCII;
     private double magnitud=0.0;
     private int size=0;
     private double[] normalizado;
     
-    public Elemento(double[] datos){
-        this.categorias=datos;
-        size=datos.length;
+    public Elemento(double[] numeros, double[][] palabras){//--------------->un arreglo para datos numericos y otro arreglo de arreglos 
+        this.datosNumericos=datos;                                        //representando la primera dimension cada palabra y la segunda
+        this.datosASCII=palabras;                                         //cada representacion de la letra en ASCII
+        size=numeros.length+palabras.length;
         Normalizar();
     }
+
+    //CAMBIAR TODO EL MODELO ACTUAL A UNO DONDE SE TOMEN EN CUENTA NUMEROS Y CARACTERES EN UN MODELO BALANCEADO
     //normaliza los datos entregados en categorias
     private void Normalizar(){
         CalcMagnitude();
