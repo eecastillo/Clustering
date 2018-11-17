dd289acd39537e4c2f262d19786a3c5c28aaa1e8
eecastillo <eduardo_ethandrake@yahoo.com.mx>
2018-11-17 14:42:01 -0600

    MarkX.1

---
+++ ClusterMin.java
@@ -0,0 +1,28 @@
+public class ClusterMin extends Cluster{
+
+    public ClusterMin(double[] numData, double[][] ASCIIData){
+        super(numData,ASCIIData);
+    }
+    public ClusterMin(ClusterMin x, ClusterMin y){
+        super(x,y);
+    }
+
+    public static double Distancia(ClusterMin x, ClusterMin y, TipoD tipo){
+       /* double[] minNumX = x.elements[0].getNumeros();
+		double[] minNumY = y.elements[0].getNumeros();
+		double[][] minASCIIX = x.elements[0].getASCII();
+        double[][] minASCIIY = y.elements[0].getASCII();*/
+        double distance=0;
+        double minDistance=Cluster.Distancia( x.elements[0].getNumeros(), y.elements[0].getNumeros(), x.elements[0].getASCII(), y.elements[0].getASCII(), tipo);
+        
+        for(int i=0;i<x.size;i++){
+            for(int j=0;j<y.size;j++){
+                distance=Cluster.Distancia(x.elements[i].getNumeros(), y.elements[j].getNumeros(), x.elements[i].getASCII(), y.elements[j].getASCII(), tipo);
+                if(distance<minDistance){
+                    minDistance=distance;
+                }
+            }
+        }
+        return minDistance;
+    }
+}
